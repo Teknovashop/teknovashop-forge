b7a3dac3c0ea9fb911bf6c6b91bd5439a958c2d1
Teknovashop <teknovashop25@gmail.com>
2025-09-13 17:45:23 +0200

    Update cable_tray.py

--- apps/stl-service/models/cable_tray.py
+++ apps/stl-service/models/cable_tray.py
@@ -1,8 +1,20 @@
-import cadquery as cq
+# models/cable_tray.py
+from typing import Dict, List
+from utils.stl_writer import Tri, add_box, triangles_to_stl
 
-def build(length=300, width=60, height=40):
-    tray = cq.Workplane("XY").rect(length, width).extrude(2)
-    side = cq.Workplane("XY").rect(length, 2).extrude(height)
-    side_r = side.translate((0, (width/2)-1, 0))
-    side_l = side.mirror('YZ').translate((0, -(width/2)+1, 0))
-    return tray.union(side_r).union(side_l)
+def build(params: Dict) -> bytes:
+    length    = float(params.get("length", 180.0))
+    inner_w   = float(params.get("inner_w", 20.0))
+    inner_h   = float(params.get("inner_h", 15.0))
+    wall_t    = float(params.get("wall_t", 2.5))
+    base_t    = float(params.get("base_t", 3.0))
+
+    tris: List[Tri] = []
+    # Base
+    add_box(tris, 0, 0, base_t/2, length, inner_w + 2*wall_t, base_t)
+    # Pared izquierda
+    add_box(tris, 0,  (inner_w/2 + wall_t/2), (base_t + inner_h/2), length, wall_t, inner_h)
+    # Pared derecha
+    add_box(tris, 0, -(inner_w/2 + wall_t/2), (base_t + inner_h/2), length, wall_t, inner_h)
+
+    return triangles_to_stl("cable_tray", tris)
