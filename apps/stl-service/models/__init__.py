71b86723cc44696cba0ff1e1de9cb64e36eff5bc
Teknovashop <teknovashop25@gmail.com>
2025-09-20 18:36:17 +0200

    Update __init__.py

--- apps/stl-service/models/__init__.py
+++ apps/stl-service/models/__init__.py
@@ -1,8 +1,19 @@
-# apps/stl-service/models/__init__.py
-from .vesa import generate_vesa_plate
+from .cable_tray import make_model as make_cable_tray
+from .router_mount import make_model as make_router_mount
+from .vesa_adapter import make_model as make_vesa
 
-MODEL_REGISTRY = {
-    "vesa": generate_vesa_plate,
-}
+# nuevos
+from .phone_stand import make_model as make_phone_stand
+from .qr_plate import make_model as make_qr_plate
+from .enclosure_ip65 import make_model as make_enclosure_ip65
+from .cable_clip import make_model as make_cable_clip
 
-__all__ = ["MODEL_REGISTRY", "generate_vesa_plate"]
+__all__ = [
+    "make_cable_tray",
+    "make_router_mount",
+    "make_vesa",
+    "make_phone_stand",
+    "make_qr_plate",
+    "make_enclosure_ip65",
+    "make_cable_clip",
+]
