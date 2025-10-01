834ca97b886d88c0de1972a6eccda3fd316d5bba
Teknovashop <teknovashop25@gmail.com>
2025-10-01 19:00:44 +0200

    Update asgi.py

--- apps/stl-service/asgi.py
+++ apps/stl-service/asgi.py
@@ -1,7 +1,4 @@
-# teknovashop-forge/asgi.py
-"""
-ASGI entrypoint para Uvicorn/Render.
-Permite usar `uvicorn asgi:app` o `uvicorn asgi:fastapi_app`.
-"""
+# /asgi.py
 from app import app as fastapi_app
+
 app = fastapi_app
